dfa58ff86c6d1850d03562486b5332e4f3bebf10
Harry Clark <139020900+astonHC@users.noreply.github.com>
2024-08-13 13:27:48 +0100

    Update 68K.h

--- inc/68K.h
+++ inc/68K.h
@@ -17,9 +17,12 @@
 
 /* SYSTEM INCLUDES */
 
+#include <assert.h>
 #include <malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdarg.h>
 #include <string.h>
 
@@ -35,9 +38,37 @@
     #include "68KCONF.h"
 #endif
 
-/*---------------------------------------------*/
-/*               68000 CPU TYPES               */
-/*---------------------------------------------*/
+/*===============================================================================*/
+/*							68000 MAIN CPU FUNCTIONALIY							 */
+/*===============================================================================*/
+
+#define         M68K_ADDRESS_WIDTH_8            0xFF
+#define         M68K_ADDRESS_WIDTH_16           0xFFFF
+#define         M68K_ADDRESS_WIDTH_32           0xFFFFFFFF
+#define         M68K_SYMBOL_WIDTH               ' '
+#define         M68K_ADDRESS_IA                 (0x8000000 << 0xDFFFFFF)
+
+/*===============================================================================*/
+/*-------------------------------------------------------------------------------*/
+/*                          68000 MEMORY UTILITY MACROS                          */
+/*                      FOR READING THE BYTE ORDER OF MEMORY                     */
+/*-------------------------------------------------------------------------------*/
+/*              THE FOLLOWING MACROS ARE DESIGNED FOR READING MEMORY             */
+/*                      FROM A PREDEFINED MEMORY MAPPER                          */
+
+/*                      BY BITSHIFTING THE CORRESPONDING BITS                    */
+/*                      TO THE DESIGNATED DATA TYPE                              */
+/*-------------------------------------------------------------------------------*/
+/*===============================================================================*/
+
+#define         M68K_READ_BYTE(VALUE, ADDRESS)              (VALUE)[(ADDRESS) ^ 1]
+#define         M68K_READ_WORD(VALUE, ADDRESS)              (((VALUE)[ADDRESS]<<8) | (VALUE)[(ADDRESS)+1])
+
+#define         M68K_READ_WORD_LONG(VALUE, ADDRESS)         (((VALUE)[(ADDRESS)+1]<<24) |       \
+                                    		                ((VALUE)[(ADDRESS)]<<16) |  		\
+                                    		                ((VALUE)[(ADDRESS )+3]<<8) |   		\
+                                    		                (VALUE)[(ADDRESS)+2])
+
 
 #define         M68K_CPU_000                0
 #define         M68K_CPU_010                1
