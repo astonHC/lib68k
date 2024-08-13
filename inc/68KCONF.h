ab641e5d9fcca4cf1f1bb9813b656fad115ca9d9
Harry Clark <139020900+astonHC@users.noreply.github.com>
2024-08-14 00:40:13 +0100

    initial config commit
    featuring external labels for ease of use in other header files

--- inc/68KCONF.h
+++ inc/68KCONF.h
@@ -4,3 +4,64 @@
 
 /* THIS FILE PERTAINS TOWARDSS THE MAIN FUNCTIONALITY SURROUNDING THE CONFIGURATION */
 /* OF EACH RESPECTIVE CPU'S TYPE AND RELATIVE HANDLERS */ 
+
+#ifndef M68K_CONFIG
+#define M68K_CONFIG
+
+/* NESTED INCLUDES */
+
+#include "common.h"
+
+/*===============================================================================*/
+/*							68000 DEBUG											 */
+/*===============================================================================*/
+
+/* MISCALLANEOUS TYPES FOR A LINE EXCEPTION HANDLERS */
+/* THIS WILL BE USED IN DOCUMENTING CYCLE ACCURATE EXCEPTIONS AT RUNTIME */
+
+#if M68K_LOG_ENABLE
+	extern FILE* M68K_LOG_FILEHANDLE
+	#define M68K_DO_LOG(A) 			if(M68K_LOG_FILEHANDLE) 
+	#if M68K_LOG_1010_1111_A_LINE
+		#define M68K_DO_LOG(A) 		if(M68K_LOG_FILEHANDLE) fprintf(stdout, A)
+	#else
+		#define M68K_DO_LOG_EMU(A)
+	#endif
+#else
+	#define M68K_DO_LOG(A)
+	#define M68K_DO_LOG_EMU(A)
+
+	/* OPTIONS FOR IMMEDIATE ADDRESSING DIRECTIVES WHEN LOOKING */
+	/* FOR CPU CALLBACKS */	
+
+	#define M68K_OPT_OFF				0
+	#define M68K_OPT_ON					1
+
+#endif
+
+
+#define 	M68K_MASK_OUT_ABOVE_8(A)            ((A) & 0xFF)
+#define 	M68K_MASK_OUT_ABOVE_16(A)           ((A) & 0xFFFF)
+#define 	M68K_MASK_OUT_ABOVE_32(A)           ((A) & 0xFFFFFFF)
+#define     M68K_RETURN_ADDRESS(ADDRESS)        ((*ADDRESS) & 0xFFFFFFFFFF)
+
+
+/*===============================================================================*/
+/*							68000 READ AND WRITE							     */
+/*===============================================================================*/
+
+extern unsigned int M68K_READ_8(unsigned int ADDRESS);
+extern unsigned int M68K_READ_16(unsigned int ADDRESS);
+extern unsigned int M68K_READ_32(unsigned int ADDRESS);
+
+extern void M68K_WRITE_8(unsigned int ADDRESS, unsigned int DATA);
+extern void M68K_WRITE_16(unsigned int ADDRESS, unsigned int DATA);
+extern void M68K_WRITE_32(unsigned int ADDRESS, unsigned int DATA);
+
+extern unsigned int CTRL_READ_BYTE(unsigned int ADDRESS);
+extern unsigned int CTRL_READ_WORD(unsigned int ADDRESS);
+extern void CTRL_WRITE_BYTE(unsigned int ADDRESS, unsigned int DATA);
+extern void CTRL_WRITE_WORD(unsigned int ADDRESS, unsigned int DATA);
+
+
+#endif
