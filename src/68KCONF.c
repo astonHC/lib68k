2776ef919705d06c73789e7f9e86b5da287e3e42
Harry Clark <139020900+astonHC@users.noreply.github.com>
2024-08-23 19:00:54 +0100

    Update 68KCONF.c

--- src/68KCONF.c
+++ src/68KCONF.c
@@ -31,6 +31,7 @@ void M68K_SET_CPU_TYPE(unsigned TYPE)
             M68K_ADDRESS_MASK += 0x00FFFFFF;
             M68K_CYCLE = CPU->INSTRUCTION_CYCLES[0];
             M68K_CYC_EXCE += CPU->CYCLE_EXCEPTION[0];
+            M68K_RESET_LVL += 256;
             return;
 
         case M68K_CPU_010:
@@ -39,12 +40,45 @@ void M68K_SET_CPU_TYPE(unsigned TYPE)
             M68K_ADDRESS_MASK += 0x00FFFFFF;
             M68K_CYCLE = CPU->INSTRUCTION_CYCLES[1];
             M68K_CYC_EXCE += CPU->CYCLE_EXCEPTION[1];
+            M68K_RESET_LVL += 256;
             return;
-        
-    
-    default:
-        break;
+
+        case M68K_CPU_020:
+            CPU_TYPE = M68K_CPU_020;
+            M68K_SR_MASK += 0xF71F;
+            M68K_ADDRESS_MASK += 0xFFFFFFFF;
+            M68K_CYCLE = CPU->INSTRUCTION_CYCLES[2];
+            M68K_CYC_EXCE += CPU->CYCLE_EXCEPTION[2];
+            M68K_RESET_LVL += 512;
+            return;
+
+        default:
+            break;
     }
+}
+
+/* EXECUTE THE MAIN INITIALISATION INSTRUCTIONS USED ON STARTUP TO */
+/* DETERMINE THE CORRESSPONDING AMOUNT OF CYCLES PER CLOCK TICK */
+
+/* SEE RESET COROUTINE: https://www.nxp.com/docs/en/reference-manual/M68000PRM.pdf#page=537 */
+
+int M68K_EXEC()
+{
+    /* DISCERN THE INITIAL CYCLE COUNT PER CLOCK TICK */
+    /* ASSUME THAT THE CLOCK CYCLES HAVE BEEN RESET UPON INITIAL BOOT */
+
+    int CYCLES = 0;
+    int CYCLE_COUNT = 0;
+
+    if(M68K_RESET_CYCLES)
+    {
+        CYCLE_COUNT += (int)M68K_RESET_CYCLES;
+    }
+
+    /* SET THE AVAILABLE CLOCK CYCLES */
+    M68K_SET_CYCLES(CYCLES);
+
+    return 0;
 }
 
 #endif
