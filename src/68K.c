be29b7d8fb25924ef8306dcfeddb746bb81aafbe
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-09-12 14:01:42 +0100

    Update 68K.c

--- src/68K.c
+++ src/68K.c
@@ -103,10 +103,10 @@ U32* M68K_GET_REGISTERS(struct CPU_68K* CPU, int REGISTER)
         case M68K_PC: return (U32*)(uintptr_t)M68K_RETURN_ADDRESS(CPU->PC);
 
 		case M68K_USP:
-			return CPU->S_FLAG;
+			return (U32*)&(CPU->S_FLAG);
 
 		case M68K_ISP:
-				return CPU->S_FLAG + CPU->STACK_POINTER[4];
+				return ((U32*)&(CPU->S_FLAG) + CPU->STACK_POINTER[4]);
 
 		case M68K_SP:
 			return CPU->REGISTER_BASE[15];
