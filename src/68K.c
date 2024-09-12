cd62028f2b902d4e980cd2b0350428208404cf2e
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-09-12 13:37:18 +0100

    updated uneeded type cast for return type

--- src/68K.c
+++ src/68K.c
@@ -103,7 +103,7 @@ U32* M68K_GET_REGISTERS(struct CPU_68K* CPU, int REGISTER)
         case M68K_PC: return (U32*)(uintptr_t)M68K_RETURN_ADDRESS(CPU->PC);
 
 		case M68K_USP:
-			return (unsigned*)CPU->S_FLAG;
+			return CPU->S_FLAG;
 
 		case M68K_ISP:
 				return CPU->S_FLAG + CPU->STACK_POINTER[4];
@@ -130,7 +130,7 @@ U32* M68K_GET_REGISTERS(struct CPU_68K* CPU, int REGISTER)
 			return CPU->INDEX_REGISTER;
 
 		default:
-			return NULL;
+			return 0;
 	}
 }
 
