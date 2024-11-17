a0ad0182f57e88912ad2c5b062c007ff62826e35
Harry Clark <139020900+astonHC@users.noreply.github.com>
2024-11-17 13:21:17 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,10 +4,28 @@
 
 /* THIS FILE PERTAINS TOWARDS THE MAIN FUNCTIONALITY OF THE PROGRAM */
 
-#include <stdio.h>
+/* NESTED INCLUDES */
 
-int main(void)
+#include "util.h"
+#include "68K.h"
+#include "68KOPCODE.h"
+
+static LIB_BASE* LIB68K;
+
+int main(int argc, char** argv)
 {
-    fprintf(stdout, "HARRY CLARK - 68K LIBRARY");
-    return 0;
+    printf("HARRY CLARK - MOTOROLA 680x0 EMULATOR\n");
+    
+    if(argc > 1)
+    {
+        char* INDEX;
+        strncpy(LIB68K->OUTPUT_PATH, argv[1], sizeof(LIB68K->OUTPUT_PATH));
+
+        // RETURNS A NEW OCCRUANCE OF A NEW LINE CHARACTER TO DENOTE THE OUTPUT PATH
+
+        for(INDEX = strchr(LIB68K->OUTPUT_PATH, '\\'); INDEX; INDEX = strchr(INDEX, '\\'))
+        {
+            *INDEX = '/';
+        }
+    }    
 }
