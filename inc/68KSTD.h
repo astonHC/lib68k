3782f13f72787496b6ce33bbaa9b4d97e1683fe5
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2024-12-03 01:06:08 +0000

    Update 68KSTD.h

--- inc/68KSTD.h
+++ inc/68KSTD.h
@@ -18,21 +18,34 @@
     #define USE_STD_LIB
 
     #define M68K_ERROR_EXIT(MESSAGE, ...)                           \
-    do                                                              \ 
-    {                                                               \
         fprintf(stderr, "Error: " MESSAGE "\n", ##__VA_ARGS__);     \
         exit(1);                                                    \
-                                                                    \
-    } while (0)
 
 
     #define M68K_PERROR_EXIT(MESSAGE, ...)                              \
-    do                                                                  \ 
-    {                                                                   \
         perror("Error: " MESSAGE "\n", ##__VA_ARGS__);                  \
         exit(1);                                                        \
-                                                                        \
-    } while (0)
+
+    // GET THE CORRESPONDING AMOUNT OF LINE PERTAINS FILE
+    // DISCARDING ANY AND ALL EOF CHARACTERS AND WEIRD TAB SPACES
+
+    int GET_LINE(char* BUFFER, int CHARS, FILE* FILE)
+    {
+        int LEN = 0;
+
+        if(fgets(BUFFER, CHARS, FILE) == NULL) return EXIT_FAILURE;
+
+        LEN = strlen(BUFFER);
+
+        if(LEN && (BUFFER[LEN - 1] == '\r'))
+        {
+            LEN--;
+            BUFFER[LEN] = 0;
+            return LEN;
+        }
+
+        return 0;
+    }
 
 
 #endif
